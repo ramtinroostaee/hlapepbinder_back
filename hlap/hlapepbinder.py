cbcb5a878060ae2e1a42108086824db7b1af3db2
Ramtin <ramtinroostaee@gmail.com>
2024-02-06 15:38:15 +0330

    thanks to hesam, all done

--- hlap/hlapepbinder.py
+++ hlap/hlapepbinder.py
@@ -4,14 +4,12 @@ from sklearn.ensemble import RandomForestClassifier
 """HLAPepBinder"""
 
 # input example
-inp_list = [[41841.86,70.500,42170.00,0.000182,197983.956993,174968.552921,50000.000000,41375.00,0.11]]
+# inp_list = [[41841.86, 70.500, 42170.00, 0.000182, 197983.956993, 174968.552921, 50000.000000, 41375.00, 0.11]]
 
 # load
 with open('hlap/HLAPepBinder_model.pkl', 'rb') as f:
     clf = pickle.load(f)
 
-def predict(hla_input):
-    return clf.predict([hla_input])
 
-#output list
-# print(y_pred1)
+def predict(hla_input):
+    return [int(d) for d in clf.predict([hla_input])]
--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-import json
-
-from typing import Union
 
 from fastapi import Request, FastAPI
 from hlap.hlapepbinder import predict
-from pydantic import BaseModel
 
 app = FastAPI()
-class Input(BaseModel):
-    input: list[float]
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
 @app.post("/hlap")
-async def get_body(input: Input):
-    print(input.input)
-    return str(predict(input.input))
+async def get_body(request: Request):
+    inp = await request.json()
+    return {
+        key: predict(value) for key, value in inp.items()
+    }
